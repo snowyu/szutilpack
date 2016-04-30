ac9070df8918d04bb3d29b047a62d2b8467743e2
Aaron Suen <warr1024@gmail.com>
2016-04-29 20:32:09 -0400

    Cleanup, fix a server->privs access escalation loophole.

--- sz_suadmin/init.lua
+++ sz_suadmin/init.lua
@@ -1,5 +1,8 @@
 local modname = minetest.get_current_modname()
 
+------------------------------------------------------------------------
+-- SALT AND HASH GENERATION
+
 -- Minetest password hashes (and password hashes in general) should have a
 -- fixed length, though the actual length may be subject to change in
 -- future versions.
@@ -44,21 +47,61 @@ end
 -- not exposed on disk, e.g. in backups.
 upgradepass(minetest.setting_save)
 
--- Try to wrap the built-in "set" chat command, so that changing the
--- su password that way will also hash it.  This prevents users with server
--- privs (who can use /set) but not necessarily /su privs from just
--- trivially reading the config to use /su.
+------------------------------------------------------------------------
+-- PROTECT PRIVILEGE-RELATED SETTINGS
+
+-- Try to wrap the built-in "set" chat command, so that:
+--	- Changing the su password will trigger a re-hash.
+--	- Users with "server" privs (who can use /set) but without "privs"
+--	  privs cannot exploit certain known settings to gain "privs" access.
 if minetest.chatcommands and minetest.chatcommands.set
 	and minetest.chatcommands.set.func then
+	local prefix = modname .. "_"
 	local oldfunc = minetest.chatcommands.set.func
-	local function postset(...)
-		upgradepass()
-		return ...
-	end
-	minetest.chatcommands.set.func = function(...)
-		return postset(oldfunc(...))
+	minetest.chatcommands.set.func = function(name, ...)
+		-- If the server user also has privs access, just allow the
+		-- setting change, and rehash as needed.
+		if minetest.check_player_privs(name, {privs = true}) then
+			local function postset(...)
+				upgradepass()
+				return ...
+			end
+			return postset(oldfunc(name, ...))
+		end
+
+		-- Wrap the built-in setting modification function to block
+		-- certain settings from being set during the execution of
+		-- this command.
+		local oldset = minetest.setting_set
+		minetest.setting_set = function(setting, ...)
+			if setting and (setting == "name"
+				or setting:sub(1, prefix:len()) == prefix) then
+				error("NEEDPRIVS")
+			end
+			return oldset(setting, ...)
+		end
+
+		-- Helper to handle result of command pcall; report our custom
+		-- error, bubble out other errors, otherwise return normally.
+		-- Restore the normal setting modification API after the command.
+		local function postset(ok, err, ...)
+			minetest.setting_set = oldset
+			if ok then
+				return err, ...
+			else
+				if not err:find("NEEDPRIVS") then
+					error(err)
+				end
+				return false, "Some settings require additional privileges to set."
+			end
+		end
+
+		return postset(pcall(oldfunc, name, ...))
 	end
 end
+
+------------------------------------------------------------------------
+-- REGISTER CHAT COMMANDS
 
 -- Helper function to add/remove the "privs" priviledge for a user.
 local function changeprivs(name, priv)
@@ -75,6 +118,9 @@ end
 minetest.register_chatcommand("su", {
 	description = "Escalate privileges by password.",
 	func = function(name, pass)
+		if minetest.check_player_privs(name, {privs = true}) then
+			return false, "You are already a superuser."
+		end
 		local hash = minetest.setting_get(modname .. "_password_hash")
 		local salt = minetest.setting_get(modname .. "_password_salt")
 		if not pass or pass == ""
@@ -95,14 +141,18 @@ minetest.register_chatcommand("unsu", {
 	func = function(name) return changeprivs(name) end
 })
 
+------------------------------------------------------------------------
+-- STRICT MODE ENFORCEMENT
+
 -- Allow a "strict" setting to be set, which requires players to use
 -- /su to escalate to admin after login, i.e. their privs are NOT persisted
 -- after logout.
-minetest.register_on_joinplayer(function(player)
+local function strictenforce(player)
+	if not minetest.setting_getbool(modname .. "_strict") then return end
 	if not player then return end
 	local name = player:get_player_name()
 	if not name then return end
-	if minetest.setting_getbool(modname .. "_strict") then
-		return changeprivs(name)
-	end
-end)
+	return changeprivs(name)
+end
+minetest.register_on_joinplayer(strictenforce)
+minetest.register_on_leaveplayer(strictenforce)
