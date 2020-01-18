46c45c8302ffda43b9899a8bdf39dc4fcc1caa78
Aaron Suen <warr1024@gmail.com>
2020-01-18 00:37:41 -0500

    Work around a reported crash.

--- szutil_stealth/init.lua
+++ szutil_stealth/init.lua
@@ -89,6 +89,7 @@ chathook()
 
 local function stripstatus(msg, ...)
 	local pref, clients = string_match(msg, "^(.*)clients={(.*)}$")
+	if not clients then return msg, ... end
 	local clist = {}
 	for pname in string_gmatch(clients, "%S+") do
 		if string_sub(pname, -1) == "," then
