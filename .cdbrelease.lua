7e630e2eb917dbc504b746299dbfb127b8706600
Aaron Suen <warr1024@gmail.com>
2021-02-27 20:10:01 -0500

    Update CDB metadata

--- .cdbrelease.lua
+++ .cdbrelease.lua
@@ -10,9 +10,19 @@ if not stamp then return end
 stamp = math_floor((stamp - 1540612800) / 60)
 stamp = ("00000000" .. stamp):sub(-8)
 
+-- luacheck: push
+-- luacheck: globals config readtext readbinary
+
+readtext = readtext or function() end
+readbinary = readbinary or function() end
+
 return {
 	user = "Warr1024",
 	pkg = "szutilpack",
 	min = "5.0",
-	version = stamp .. "-$Format:%h$"
+	version = stamp .. "-$Format:%h$",
+	long_description = readtext(".cdbdesc.md"),
+	screenshots = {readbinary(".cdbscreen.png")}
 }
+
+-- luacheck: pop
