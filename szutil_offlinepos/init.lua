aef33d9776f418b368da8055a0c770555c2e9ad6
Aaron Suen <warr1024@gmail.com>
2021-03-01 07:23:47 -0500

    Try to make offlinepos globalstep more efficient
    This is showing up prominently on profiler data

--- szutil_offlinepos/init.lua
+++ szutil_offlinepos/init.lua
@@ -1,12 +1,40 @@
 -- LUALOCALS < ---------------------------------------------------------
-local minetest, pairs, pcall, string, table
-    = minetest, pairs, pcall, string, table
-local string_gmatch, string_match, table_concat
-    = string.gmatch, string.match, table.concat
+local ipairs, math, minetest, pairs, pcall, string, table
+    = ipairs, math, minetest, pairs, pcall, string, table
+local math_random, string_gmatch, string_match, table_concat
+    = math.random, string.gmatch, string.match, table.concat
 -- LUALOCALS > ---------------------------------------------------------
 
 local modname = minetest.get_current_modname()
 local modstore = minetest.get_mod_storage()
+
+local cache = {}
+for k, v in pairs(modstore:to_table().fields) do
+	cache[k] = minetest.string_to_pos(v)
+end
+
+minetest.register_globalstep(function()
+		for _, player in ipairs(minetest.get_connected_players()) do
+			local pname = player:get_player_name()
+			local pos = player:get_pos()
+			local opos = cache[pname]
+			if (not opos) or (not vector.equals(pos, opos)) then
+				modstore:set_string(pname, minetest.pos_to_string(pos))
+				cache[pname] = pos
+			end
+		end
+	end)
+
+local function expire()
+	for k in pairs(cache) do
+		if not minetest.player_exists(k) then
+			modstore:set_string(k, "")
+			cache[k] = nil
+		end
+	end
+	minetest.after(5 + math_random() * 5, expire)
+end
+minetest.after(0, expire)
 
 minetest.register_privilege(modname, {
 		description = "Can see position of other players",
@@ -16,8 +44,7 @@ minetest.register_privilege(modname, {
 
 local function matchplayers(spec)
 	local found = {}
-	local fields = modstore:to_table().fields
-	for k, v in pairs(fields) do
+	for k, v in pairs(cache) do
 		local matched
 		for p in string_gmatch(spec, "[^%s]+") do
 			if not matched then
@@ -29,7 +56,7 @@ local function matchplayers(spec)
 			local player = minetest.get_player_by_name(k)
 			found[k] = {
 				name = k,
-				pos = player and player:get_pos() or minetest.string_to_pos(v),
+				pos = player and player:get_pos() or v,
 				player = player
 			}
 		end
@@ -52,26 +79,6 @@ minetest.register_chatcommand("pos", {
 		end
 	})
 
-local storetbl = modstore:to_table()
-minetest.register_globalstep(function()
-		local dirty
-		for _, player in pairs(minetest.get_connected_players()) do
-			local pname = player:get_player_name()
-			local pstr = minetest.pos_to_string(player:get_pos())
-			if storetbl.fields[pname] ~= pstr then
-				storetbl.fields[pname] = pstr
-				dirty = true
-			end
-		end
-		for k in pairs(storetbl.fields) do
-			if not minetest.player_exists(k) then
-				storetbl.fields[k] = nil
-				dirty = true
-			end
-		end
-		if dirty then modstore:from_table(storetbl) end
-	end)
-
 local teleport = minetest.registered_chatcommands.teleport
 if teleport and teleport.func then
 	local oldfunc = teleport.func
@@ -84,12 +91,10 @@ if teleport and teleport.func then
 		function minetest.get_player_by_name(name, ...)
 			local player = oldget(name, ...)
 			if player then return player end
-			local s = modstore:get_string(name)
-			if s and s ~= "" then
+			local s = cache[name]
+			if s then
 				return {
-					get_pos = function()
-						return minetest.string_to_pos(s)
-					end,
+					get_pos = function() return s end,
 					set_pos = function() end
 				}
 			end
